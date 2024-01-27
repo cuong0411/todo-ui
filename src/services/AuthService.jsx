2bda543dc456cad8c730236fe58c7f2a50fe81d6
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2024-01-27 11:47:04 +0700

    update url

--- src/services/AuthService.jsx
+++ src/services/AuthService.jsx
@@ -1,26 +1,27 @@
-import axios from "axios";
+import axios from 'axios';
 
-const AUTH_REST_API_BASE_URL = "http://localhost:8080/api/auth";
+const AUTH_REST_API_BASE_URL =
+  'https://todo-list-demo-ab0a43442d6a.herokuapp.com/api/auth';
 
 export const registerApiCall = (registerObj) =>
-  axios.post(AUTH_REST_API_BASE_URL + "/register", registerObj);
+  axios.post(AUTH_REST_API_BASE_URL + '/register', registerObj);
 
 export const loginApiCall = (loginObj) =>
-  axios.post(AUTH_REST_API_BASE_URL + "/login", loginObj);
+  axios.post(AUTH_REST_API_BASE_URL + '/login', loginObj);
 
-export const storeToken = (token) => localStorage.setItem("token", token);
-export const getToken = () => localStorage.getItem("token");
+export const storeToken = (token) => localStorage.setItem('token', token);
+export const getToken = () => localStorage.getItem('token');
 
 export const saveLoggedInUser = (username, role) => {
-  sessionStorage.setItem("authenticatedUser", username);
-  sessionStorage.setItem("role", role);
+  sessionStorage.setItem('authenticatedUser', username);
+  sessionStorage.setItem('role', role);
 };
 export const isUserLoggedIn = () => {
-  const username = sessionStorage.getItem("authenticatedUser");
+  const username = sessionStorage.getItem('authenticatedUser');
   return !(username === null);
 };
 export const getLoggedInUser = () =>
-  sessionStorage.getItem("authenticatedUser");
+  sessionStorage.getItem('authenticatedUser');
 
 export const logout = () => {
   localStorage.clear();
@@ -28,6 +29,6 @@ export const logout = () => {
 };
 
 export const isAdminUser = () => {
-  let role = sessionStorage.getItem("role");
-  return role !== null && role === "ROLE_ADMIN";
+  let role = sessionStorage.getItem('role');
+  return role !== null && role === 'ROLE_ADMIN';
 };
